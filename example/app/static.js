b0ff766ce24438da579902c10b489b2c3dbbc0f4
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-28 03:05:00 +0000

    Use browserify-middleware to compile app.

--- example/app/static.js
+++ example/app/static.js
@@ -1,7 +1,10 @@
 
 var path = require('path');
 var express = require('express');
+var browserify = require('browserify-middleware');
+
 module.exports = function (app) {
     app.use(express.static(path.join(__dirname, '..', 'public')));
+    app.get('/app.js', browserify(__dirname + '/../client/app.js'));
     return app;
 };
